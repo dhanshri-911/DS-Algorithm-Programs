fc7d9197ad0c3afa00d09bcb53fcc9c612d8082d
Dhanshri911 <dszingade911@gmail.com>
2022-03-30 19:30:08 +0530

    Permutations

---
+++ src/Permutations.java
@@ -0,0 +1,18 @@
+public class Permutations {
+    static void printPermutatn(String  str,String  ans) {
+        if (str.length() == 0) {
+            System.out.println(ans + " ");
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            //ith char of string
+            char ch = str.charAt(i);
+            String ros = str.substring(0, i) + str.substring(i + 1);
+            printPermutatn(ros, ans + ch);
+        }
+    }
+        public static void main(String[] args){
+            String s = "abb";
+            printPermutatn(s, " ");
+        }
+}
